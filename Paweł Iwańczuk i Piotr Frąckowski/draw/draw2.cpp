772436d587bfd059665479629008102b8e129528
Piotr <p.frackowski12191@gmail.com>
2019-06-01 15:56:50 +0200

    add base

---
+++ Paweł Iwańczuk i Piotr Frąckowski/draw/draw2.cpp
@@ -0,0 +1,556 @@
+// draw.cpp : Defines the entry point for the application.
+//
+
+#include "stdafx.h"
+#include "draw2.h"
+#include <vector>
+#include <cstdio>
+#include <fstream>
+#include <string>
+#include <sstream> 
+#include <algorithm>
+#include <cmath>
+#include <chrono>
+
+#define MAX_LOADSTRING 100
+#define TMR_1 1
+
+// Global Variables:
+HINSTANCE hInst;								// current instance
+TCHAR szTitle[MAX_LOADSTRING];					// The title bar text
+TCHAR szWindowClass[MAX_LOADSTRING];			// the main window class name
+
+INT value;
+int pomijanie;
+double skala=1;
+int wys = 10;
+int s=800;
+int pomin = 161;
+double okres = 0;
+double g_actual = 0;
+bool okr = false;
+bool prz = false;
+bool del = false;
+
+// buttons
+HWND hwndButton;
+
+// sent data
+int col = 0;
+std::vector<Point> data;
+std::vector < int > okresr;
+RECT drawArea1 = { 0, 0, 150, 200 };
+RECT drawArea2 = { 50, 400, 650, 422};
+
+// Forward declarations of functions included in this code module:
+ATOM				MyRegisterClass(HINSTANCE hInstance);
+BOOL				InitInstance(HINSTANCE, int);
+LRESULT CALLBACK	WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK	About(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK	Buttons(HWND, UINT, WPARAM, LPARAM);
+
+
+void MyOnPaint(HDC hdc)
+{
+	Graphics graphics(hdc);
+	Pen pen(Color(255, 0, 0, 255));
+	Pen pen2(Color(255, 25*col, 0, 255));
+	Gdiplus::SolidBrush bialy(Gdiplus::Color(255, 255, 255, 255));
+	Gdiplus::Font* czcionka = new Font(L"Times New Roman", 13);
+
+	PointF B = PointF(250, 5);
+	PointF D = PointF(250, 35);
+	Gdiplus::SolidBrush cyfry(Gdiplus::Color(255, 0, 0, 0));
+
+	for (int i = (pomin + 1); i < (s); i++) {
+		graphics.DrawLine(&pen2, data[i - 1].X * skala, data[i - 1].Y * wys / 10, data[i].X * skala, data[i].Y * wys / 10);
+		for(int j=1; j< okresr.size(); j++)
+		{
+			if (i > okresr[j])
+			{
+				okres = (okresr[j] - okresr[j - 1])*0.04;
+			}
+		}
+		graphics.FillRectangle(&bialy, 245, 0, 200, 60);
+
+		if (okr == true) {
+			std::wstring liczba_string;
+			liczba_string = std::to_wstring(okres);
+			graphics.DrawString(liczba_string.c_str(), liczba_string.length(), czcionka, B, &cyfry);
+		}
+		if (prz == true) {
+			std::wstring liczba_string1;
+			liczba_string1 = std::to_wstring(g_actual*9.81);
+			graphics.DrawString(liczba_string1.c_str(), liczba_string1.length(), czcionka, D, &cyfry);
+		}
+		if (del == true) {
+			_sleep(1);
+		}
+	}
+}
+
+void rysowanieOkresu(HDC hdc)
+{
+	Graphics graphics(hdc);
+
+	Gdiplus::Font* czcionka = new Font(L"Times New Roman", 13);
+	PointF A = PointF(5, 5);
+	PointF C = PointF(5, 35);
+	PointF E = PointF(5, 65);
+	PointF F = PointF(250, 65);
+
+	Gdiplus::SolidBrush cyfry(Gdiplus::Color(255, 0, 0, 0));
+	graphics.DrawString(L"Okres ostatniego wahniêcia (s):", 31, czcionka, A, &cyfry);
+	graphics.DrawString(L"Przyspieszenie ziemskie (m/s^2):", 32, czcionka, C, &cyfry);
+	graphics.DrawString(L"Liczba pomijanych probek:", 32, czcionka, E, &cyfry);
+	std::wstring liczba_string2;
+	liczba_string2 = std::to_wstring(pomin-1);
+	graphics.DrawString(liczba_string2.c_str(), liczba_string2.length(), czcionka, F, &cyfry);
+
+}
+
+
+void repaintWindow(HWND hWnd, HDC &hdc, PAINTSTRUCT &ps, RECT *drawArea)
+{
+	if (drawArea==NULL)
+		InvalidateRect(hWnd, NULL, TRUE); // repaint all
+	else
+		InvalidateRect(hWnd, drawArea, TRUE); //repaint drawArea
+	hdc = BeginPaint(hWnd, &ps);
+	rysowanieOkresu(hdc);
+	MyOnPaint(hdc);
+	EndPaint(hWnd, &ps);
+}
+
+class pomiar
+{
+public:
+	float ax;
+	float ay;
+	float az;
+	pomiar(float Ax, float Ay, float Az);
+};
+pomiar::pomiar(float Ax, float Ay, float Az)
+	: ax(Ax)
+	, ay(Ay)
+	, az(Az)
+{
+}
+
+void inputData()
+{	
+	std::fstream dane;
+	std::string linia;
+	dane.open("outputPendulum01.log", std::ios::in);
+	float a, b, c;
+	std::vector < pomiar > przysp;
+	if (dane.good() == true)
+	{
+		while (!dane.eof())
+		{
+			getline(dane, linia);
+			std::stringstream p(linia);
+			for (int i = 0; i < 7; i++) {
+				p >> a;
+			}
+			p >> b;
+			p >> c;
+			przysp.push_back(pomiar(a, b, c));
+		}
+		dane.close();
+	}
+	else
+	{
+		//blad pliku
+	}
+	//tutaj zaczyna sie obliczanie przyspieszenia
+	long float xx, yy, zz;
+	xx = 0;
+	yy = 0;
+	zz = 0;
+	int s;
+	s = przysp.size();
+	for (int i = 0; i < s; i++) {
+		xx = xx + przysp[i].ax;
+		yy = yy + przysp[i].ay;
+		zz = zz + przysp[i].az;
+	}
+	xx = xx / s;
+	yy = yy / s;
+	zz = zz / s;
+	g_actual = sqrt((xx * xx + yy * yy + zz * zz));
+	std::vector < pomiar > wysw;
+	for (int i = 0; i < s; i++) {
+		a = (xx * ((przysp[i].ax * xx + przysp[i].ay * yy + przysp[i].az * zz) / (g_actual * g_actual))) - xx;
+		b = (yy * ((przysp[i].ax * xx + przysp[i].ay * yy + przysp[i].az * zz) / (g_actual * g_actual))) - yy;
+		c = (zz * ((przysp[i].ax * xx + przysp[i].ay * yy + przysp[i].az * zz) / (g_actual * g_actual))) - zz;
+		wysw.push_back(pomiar(a, b, c));
+	}
+	std::vector < float > wart;
+	float f;
+	float d = 0;
+	//d = (-d / 2) * 100.0;
+	d = 0;
+	for (int i = 0; i < s; i++) {
+		f = sqrt(wysw[i].ax * wysw[i].ax + wysw[i].ay * wysw[i].ay + wysw[i].az * wysw[i].az);
+		if (wysw[i].ax > 0)
+		{
+			f = -f;
+		}
+		d = d + (f * 9.81 * 0.04 * 0.04 / 2.0) * 100.0;
+		wart.push_back(d);
+	}
+	okresr.push_back(0);
+	for (int i = 2; i < s - 2; i++) {
+		if (wart[i] > wart[i - 1] && wart[i] > wart[i - 2] && wart[i] > wart[i + 1] && wart[i] > wart[i + 2])
+		{
+			okresr.push_back(i);
+		}
+	}
+	data.push_back(Point(0, 0));
+	for (int i = 1; i < s; i++){
+		data.push_back(Point((i/2)/skala+1, (wart[i]*10+100)+i/20));
+	}
+	s = data.size();
+}
+
+
+int OnCreate(HWND window)
+{
+	inputData();
+	return 0;
+}
+
+
+
+// main function (exe hInstance)
+int APIENTRY _tWinMain(HINSTANCE hInstance,
+	HINSTANCE hPrevInstance,
+	LPTSTR    lpCmdLine,
+	int       nCmdShow)
+{
+	UNREFERENCED_PARAMETER(hPrevInstance);
+	UNREFERENCED_PARAMETER(lpCmdLine);
+
+	// TODO: Place code here.
+	
+	MSG msg;
+	HACCEL hAccelTable;
+
+	value = 0;
+
+	GdiplusStartupInput gdiplusStartupInput;
+	ULONG_PTR           gdiplusToken;
+	GdiplusStartup(&gdiplusToken, &gdiplusStartupInput, NULL);
+
+	// Initialize global strings
+	LoadString(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+	LoadString(hInstance, IDC_DRAW, szWindowClass, MAX_LOADSTRING);
+	MyRegisterClass(hInstance);
+
+
+
+	// Perform application initialization:
+	if (!InitInstance(hInstance, nCmdShow))
+	{
+		return FALSE;
+	}
+
+	hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_DRAW));
+
+	// Main message loop:
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+	}
+
+	GdiplusShutdown(gdiplusToken);
+
+	return (int)msg.wParam;
+}
+
+
+
+//
+//  FUNCTION: MyRegisterClass()
+//
+//  PURPOSE: Registers the window class.
+//
+//  COMMENTS:
+//
+//    This function and its usage are only necessary if you want this code
+//    to be compatible with Win32 systems prior to the 'RegisterClassEx'
+//    function that was added to Windows 95. It is important to call this function
+//    so that the application will get 'well formed' small icons associated
+//    with it.
+//
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+	WNDCLASSEX wcex;
+
+	wcex.cbSize = sizeof(WNDCLASSEX);
+
+	wcex.style = CS_HREDRAW | CS_VREDRAW;
+	wcex.lpfnWndProc = WndProc;
+	wcex.cbClsExtra = 0;
+	wcex.cbWndExtra = 0;
+	wcex.hInstance = hInstance;
+	wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_DRAW));
+	wcex.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	wcex.lpszMenuName = MAKEINTRESOURCE(IDC_DRAW);
+	wcex.lpszClassName = szWindowClass;
+	wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+	return RegisterClassEx(&wcex);
+}
+
+//
+//   FUNCTION: InitInstance(HINSTANCE, int)
+//
+//   PURPOSE: Saves instance handle and creates main window
+//
+//   COMMENTS:
+//
+//        In this function, we save the instance handle in a global variable and
+//        create and display the main program window.
+//
+
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+	HWND hWnd;
+
+
+	hInst = hInstance; // Store instance handle (of exe) in our global variable
+
+	// main window
+	hWnd = CreateWindow(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, NULL, NULL, hInstance, NULL);
+
+	// create button and store the handle           
+	
+	
+	hwndButton = CreateWindow(TEXT("button"),                      // The class name required is button
+		TEXT("DrawAll"),                  // the caption of the button
+		WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,  // the styles
+		500, 0,                                  // the left and top co-ordinates
+		80, 110,                              // width and height
+		hWnd,                                 // parent window handle
+		(HMENU)ID_BUTTON2,                   // the ID of your button
+		hInstance,                            // the instance of your application
+		NULL);                               // extra bits you dont really need
+
+	hwndButton = CreateWindow(TEXT("button"),                      // The class name required is button
+		TEXT("Zwiêksz skalê czasu"),                  // the caption of the button
+		WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,  // the styles
+		600, 0,                                  // the left and top co-ordinates
+		180, 50,                              // width and height
+		hWnd,                                 // parent window handle
+		(HMENU)ID_BUTTON3,                   // the ID of your button
+		hInstance,                            // the instance of your application
+		NULL);
+	hwndButton = CreateWindow(TEXT("button"),                      // The class name required is button
+		TEXT("Zmniejsz skalê czasu"),                  // the caption of the button
+		WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,  // the styles
+		600, 60,                                  // the left and top co-ordinates
+		180, 50,                              // width and height
+		hWnd,                                 // parent window handle
+		(HMENU)ID_BUTTON4,                   // the ID of your button
+		hInstance,                            // the instance of your application
+		NULL);
+	hwndButton = CreateWindow(TEXT("button"),                      // The class name required is button
+		TEXT("Zmniejsz skalê wysokosci"),                  // the caption of the button
+		WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,  // the styles
+		500, 120,                                  // the left and top co-ordinates
+		280, 50,                              // width and height
+		hWnd,                                 // parent window handle
+		(HMENU)ID_BUTTON5,                   // the ID of your button
+		hInstance,                            // the instance of your application
+		NULL);
+	hwndButton = CreateWindow(TEXT("button"),                      // The class name required is button
+		TEXT("Zwiêksz skalê wysokosci"),                  // the caption of the button
+		WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,  // the styles
+		500, 180,                                  // the left and top co-ordinates
+		280, 50,                              // width and height
+		hWnd,                                 // parent window handle
+		(HMENU)ID_BUTTON6,                   // the ID of your button
+		hInstance,                            // the instance of your application
+		NULL);
+	hwndButton = CreateWindow(TEXT("button"),                      // The class name required is button
+		TEXT("Pomiñ -"),                  // the caption of the button
+		WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,  // the styles
+		500, 240,                                  // the left and top co-ordinates
+		80, 50,                              // width and height
+		hWnd,                                 // parent window handle
+		(HMENU)ID_BUTTON7,                   // the ID of your button
+		hInstance,                            // the instance of your application
+		NULL);
+	hwndButton = CreateWindow(TEXT("button"),                      // The class name required is button
+		TEXT("Pomiñ +"),                  // the caption of the button
+		WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,  // the styles
+		600, 240,                                  // the left and top co-ordinates
+		80, 50,                              // width and height
+		hWnd,                                 // parent window handle
+		(HMENU)ID_BUTTON8,                   // the ID of your button
+		hInstance,                            // the instance of your application
+		NULL);
+
+	// create button and store the handle                                                       
+
+	hwndButton = CreateWindow(TEXT("button"), TEXT("Wyswietlaj okres"),
+		WS_CHILD | WS_VISIBLE | BS_AUTOCHECKBOX,
+		500, 300, 200, 30, hWnd, (HMENU)ID_CHECKBOX1, GetModuleHandle(NULL), NULL);
+
+	hwndButton = CreateWindow(TEXT("button"), TEXT("Wyswietlaj przyspieszenie"),
+		WS_CHILD | WS_VISIBLE | BS_AUTOCHECKBOX,
+		500, 350, 200, 30, hWnd, (HMENU)ID_CHECKBOX2, GetModuleHandle(NULL), NULL);
+
+	hwndButton = CreateWindow(TEXT("button"), TEXT("Delay"),
+		WS_CHILD | WS_VISIBLE | BS_AUTOCHECKBOX,
+		500, 400, 200, 30, hWnd, (HMENU)ID_CHECKBOX3, GetModuleHandle(NULL), NULL);
+
+	OnCreate(hWnd);
+
+	if (!hWnd)
+	{
+		return FALSE;
+	}
+
+	ShowWindow(hWnd, nCmdShow);
+	UpdateWindow(hWnd);
+
+	return TRUE;
+}
+
+//
+//  FUNCTION: WndProc(HWND, UINT, WPARAM, LPARAM)
+//
+//  PURPOSE:  Processes messages for the main window.
+//
+//  WM_COMMAND	- process the application menu
+//  WM_PAINT	- Paint the main window (low priority)
+//  WM_DESTROY	- post a quit message and return
+//
+//
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	int wmId, wmEvent;
+	PAINTSTRUCT ps;
+	HDC hdc;
+
+	switch (message)
+	{
+	case WM_COMMAND:
+		wmId = LOWORD(wParam);
+		wmEvent = HIWORD(wParam);
+
+		// MENU & BUTTON messages
+		// Parse the menu selections:
+		switch (wmId)
+		{
+		case IDM_ABOUT:
+			DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+			break;
+		case IDM_EXIT:
+			DestroyWindow(hWnd);
+			break;
+		case ID_BUTTON2 :
+			repaintWindow(hWnd, hdc, ps, NULL);
+			break;
+		case ID_BUTTON3:
+			if (skala > 0.4) {
+				skala = skala / 1.3;
+			}
+			break;
+		case ID_BUTTON4:
+			if (skala < 1.1) {
+				skala = skala * 1.2;
+			}
+			break;
+		case ID_BUTTON5:
+			if (wys < 25) {
+				wys = wys + 2;
+			}
+			break;
+		case ID_BUTTON6:
+			if (wys >= 7) {
+				wys = wys - 2;
+			}
+		case ID_BUTTON7:
+			if (pomin>=20) {
+				pomin = pomin - 20;
+			}
+			break;
+		case ID_BUTTON8:
+			pomin = pomin + 20;
+			break;
+		case ID_CHECKBOX1:
+			if (SendDlgItemMessage(hWnd, ID_CHECKBOX1, BM_GETCHECK, 0, 0))
+				okr=true;
+			else
+				okr=false;
+		break;
+		case ID_CHECKBOX2:
+			if (SendDlgItemMessage(hWnd, ID_CHECKBOX2, BM_GETCHECK, 0, 0))
+				prz = true;
+			else
+				prz = false;
+			break;
+		case ID_CHECKBOX3:
+			if (SendDlgItemMessage(hWnd, ID_CHECKBOX3, BM_GETCHECK, 0, 0))
+				del = true;
+			else
+				del = false;
+			break;
+		default:
+			return DefWindowProc(hWnd, message, wParam, lParam);
+		}
+		break;
+	case WM_PAINT:
+		hdc = BeginPaint(hWnd, &ps);
+		// TODO: Add any drawing code here (not depend on timer, buttons)
+		EndPaint(hWnd, &ps);
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+
+	case WM_TIMER:
+		switch (wParam)
+		{
+		case TMR_1:
+			//force window to repaint
+			repaintWindow(hWnd, hdc, ps, &drawArea2);
+			value++;
+			break;
+		}
+
+	default:
+		return DefWindowProc(hWnd, message, wParam, lParam);
+	}
+	return 0;
+}
+
+// Message handler for about box.
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	UNREFERENCED_PARAMETER(lParam);
+	switch (message)
+	{
+	case WM_INITDIALOG:
+		return (INT_PTR)TRUE;
+
+	case WM_COMMAND:
+		if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+		{
+			EndDialog(hDlg, LOWORD(wParam));
+			return (INT_PTR)TRUE;
+		}
+		break;
+	}
+	return (INT_PTR)FALSE;
+}
